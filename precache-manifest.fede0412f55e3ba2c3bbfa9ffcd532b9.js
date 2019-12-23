beb32fc8b1c2fea38d475fb8c4c225050fc4de00
Dilip Konigiri <dkonigiri@netflix.com>
2019-12-23 10:58:19 -0800

    Updates

---
+++ precache-manifest.fede0412f55e3ba2c3bbfa9ffcd532b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f658dc9765681d6ea3504acffee0a58",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e0afb8b8cc3c26f887f1",
+    "url": "/monsters-rolodex/static/css/main.10f77f47.chunk.css"
+  },
+  {
+    "revision": "7e894316149ac95bff0e",
+    "url": "/monsters-rolodex/static/js/2.994afdca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.994afdca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0afb8b8cc3c26f887f1",
+    "url": "/monsters-rolodex/static/js/main.7bc60494.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
